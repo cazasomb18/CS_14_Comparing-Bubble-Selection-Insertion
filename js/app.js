adec90a02ab58727d69bd9d8d866350223679be1
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-02-26 16:28:08 -0600

    section complete

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//==================Section 14: Comparing Bubble, Selection and Insertion Sort==================
+
+//Big 0 Sorting Algorithms
+
+//Algorithm 		Time Complexity(Best)		Time Complexity(Avg)		Time Complexity(Worst)		Space Complexity
+
+//Bubble Sort 		0(n)						O(n^2)						O(n^2)						O(1)
+//Insertion Sort 	0(n)						O(n^2)						O(n^2)						O(1)
+//Selection Sort 	O(n^2)						O(n^2)						O(n^2)						O(1)
+
+	//Bubble sort and Insertion Sort aren't bad if the data is NEARLY SORTED
+	//Selection sort goes through the entire array regardless if the data is nearly sorted or not\
+
+
+	//The Space Complexity is all the same because we aren't using much space, we aren't creating new 
+	//arrays, we aren't creating variables, or multiple variables for each element.
+
+		//Check sorting algorithms animations and compare these three algorithms on nearly sorted data
+
+	//Insertion Sort is a good option for live or constantly changing data as it sorts from beginning 
+	//to end regardless of previous sort
+
+//Recap
+	//Sorting is fundamental
+	//Bubble sort, selection sort, and insertion sort are all roughly equivalent
+	//All have average time complexities that are quadratic / O(n^2)
+	//We can do better... but we need more complex algorithms!
+
